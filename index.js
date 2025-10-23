75f136fd68e9608130d5e4b6392d6ce3a505430f
ZairyMod <122165768+ZairyMod@users.noreply.github.com>
2025-10-23 13:01:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,34 +15,34 @@ client.on("ready", async () => {
   client.user.setHypeSquad('HOUSE_BRILLIANCE');
   client.user.setPresence({ activities: [r],});
 	const {joinVoiceChannel} = require('@discordjs/voice');
-	const channel = client.channels.cache.get("Your Voice Channels ID"); // voice channel's id
+	const channel = client.channels.cache.get("1309115764476739627"); // voice channel's id
 	if (!channel) return console.log("The channel does not exist !");
 	setInterval(() => {
 			const connection = joinVoiceChannel({
 				channelId: channel.id, // the voice channel's id
 				guildId: channel.guild.id, // the guild that the channel is in
 				adapterCreator: channel.guild.voiceAdapterCreator, // and setting the voice adapter creator
-				selfDeaf: false,
+				selfDeaf: true,
 				selfMute: true,
 			});
 		  }, 6000)
 		});   
     
 const r = new RichPresence() 
-	.setApplicationId('1159018870401675276')
+	.setApplicationId('1354631077045473324')
 	.setType('PLAYING')
-	.setURL('link')
-	.setState('Set State')
-	.setName('Set Name')
-	.setDetails('Set Details')
+	.setURL('https://guns.lol/24kpablo')
+	.setState('PlayStation 5')
+	.setName('EA Sports FC 26')
+	.setDetails('Manager Career')
 	.setParty({
 		max: 5,
 		current: 3,
 		id: getUUID(),
 	})
 	.setStartTimestamp(Date.now())
-	.setAssetsLargeImage('Assets ID')
-	.setAssetsLargeText('Set Description Assets')
-	.addButton('Custom Button', 'Button Link')
+	.setAssetsLargeImage('https://cdn.discordapp.com/attachments/1430509792497242232/1430682876340469852/2e757ffb0a6bb4b91af84db64e0183d725e56e5354f45eba.png?ex=68faaac2&is=68f95942&hm=765b9b4cf4cb7a5d0f1cf97d2e325286bc06173afc84e520831c85b9f83a08d8&')
+	.setAssetsLargeText('EA Sports FC 26 (Standard Edition)')
+	.addButton('Gun.LoL', 'https://guns.lol/24kpablo')
 
 client.login(process.env.TOKEN)
